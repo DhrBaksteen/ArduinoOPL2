e20ac77429f1cdb64b3ab5fb802ff4c09f06fa0f
Maarten Janssen <maarten@cheerful.nl>
2017-11-04 13:13:33 +0100

    Adding VGM and VGZ support to OPL2Play
    The OPL2Play demo program for Raspberry Pi can now also play VGM and VGZ files. Only YM3812 commands in these files are supported. Cleanup of the OPL2Play source is needed, but I just want to get this out there for now :)

--- examples_pi/opl2play/opl2play.h
+++ examples_pi/opl2play/opl2play.h
@@ -4,32 +4,47 @@
 	#include <stdio.h>
 
 
-	struct DRO {
+	typedef struct DRO {
 		FILE *file;
-		long songLength;
+		unsigned long songLength;
 		unsigned char codeShortDelay;
 		unsigned char codeLongDelay;
 		unsigned char registerMapLength;
 		unsigned char registerMap[256];
-	};
+	} DRO;
 
 
-	struct IMF {
+	typedef struct IMF {
 		FILE *file;
-		long songLength;
+		unsigned long songLength;
 		int speed;
-	};
+	} IMF;
+	
+	
+	typedef struct VGM {
+		FILE *file;
+		unsigned long songLength;
+		unsigned long loopOffset;
+		unsigned long loopLength;
+	} VGM;
 
 
 	int main(int argc, char **argv);
-	struct DRO loadDro(FILE *droFile);
-	struct IMF loadImf(FILE *imfFile, int speed);
-	void playDroMusic(struct DRO dro);
-	void playImfMusic(struct IMF imf);
-	int fileRead(FILE *file);
+	DRO loadDro(FILE *droFile);
+	IMF loadImf(FILE *imfFile, int speed);
+	VGM loadVgm(FILE *vgmFile);
+	VGM loadVgz(FILE *vgzFile);
+	void playDroMusic(DRO dro);
+	void playImfMusic(IMF imf);
+	void playVgmMusic(VGM vgm);
+	unsigned char fileRead(FILE *file);
+	unsigned int readWord(FILE *file);
+	unsigned int readDWord(FILE *file);
 	int spiError();
 	int fileError();
 	void printHeader();
 	void showHelp();
 	void showConnections();
+	
+	int inflate(FILE *source, FILE *dest);
 #endif
