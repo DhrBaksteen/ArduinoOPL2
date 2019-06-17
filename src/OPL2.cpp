9abdf9e4e24b1709bc95c2bf8714d646497396e1
Maarten Janssen <maarten@cheerful.nl>
2019-06-17 21:09:57 +0200

    Updates for release 1.4.6

--- src/OPL2.cpp
+++ src/OPL2.cpp
@@ -35,7 +35,7 @@
  * IMPORTANT: Make sure you set the correct BOARD_TYPE in OPL2.h. Default is set to Arduino.
  *
  *
- * Last updated 2019-02-25
+ * Last updated 2019-06-17
  * Most recent version of the library can be found at my GitHub: https://github.com/DhrBaksteen/ArduinoOPL2
  * Details about the YM3812 and OPL chips can be found at http://www.shikadi.net/moddingwiki/OPL_chip
  *
